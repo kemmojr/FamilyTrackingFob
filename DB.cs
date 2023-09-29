5b8277a8ae064269059422f0d13762dd7fd6f36f
Tim Kemmis <kemmojr@gmail.com>
2023-09-29 23:54:36 +1000

    Began implementing stage 1 of the project with the beginnings of the basic design including EF and MVC design

---
+++ DB.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+public class DB
+{
+	public DbSet<MovementPacket> Packets { get; set; }
+	public DB()
+	{
+	}
+}
---
+++ MovementPacket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MovementPacket
+{
+	public MovementPacket()
+	{
+	}
+
+	//The time the client device sent this data
+	public DateTime SentDateTime { get; set; }
+
+	//The time the Start position was recorded
+	public DateTime StartDateTime { get; set; }
+
+	//The time the end position was recorded
+	public DateTime EndDateTime { get; set; }
+
+
+	//Latitude of the start position
+	public double StartLatitude { get; set; }
+
+    //Longitude of the start position
+    public double StartLongitude { get; set; }
+
+    //Latitude of the destination position
+    public double EndLatitude { get; set; }
+
+    //Longitude of the destination position
+    public double EndLongitude { get; set; }
+
+
+    /*
+		startLatitude	double	Latitude of the start position
+		startLongitude	double	Longitude of the start position
+		endLatitude	double	Latitude of the destination position
+		endLongitude	double	Longitude of the destination position
+	 * */
+}
